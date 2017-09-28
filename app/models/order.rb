ec80576807123bc0dfaa3402d30657d0d446ea24
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-09-29 00:06:57 +0300

    Added scope get Orders for user by period

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,5 @@ class Order < ApplicationRecord
   belongs_to :dish
   belongs_to :period
 
-
+  scope :for_user_by_period, -> (user_id, period_id) { where(user_id: user_id, period_id: period_id) }
 end
