6f998c70ab198a4640dd2e3a249332b44aadae33
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-09-28 09:53:59 +0300

    Added login helper for nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_helper(styles = '')
+    if current_user
+      link_to "Logout", destroy_user_session_path, method: :delete, class: styles
+    else
+      (link_to "Registered", new_user_registration_path, class: styles) +
+          " ".html_safe +
+          (link_to "Login", new_user_session_path, class: styles)
+    end
+  end
 end
