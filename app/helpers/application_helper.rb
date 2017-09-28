b4a56c2f8cbd63aa9e1acbf962eab0bcaad5141d
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-09-28 13:15:54 +0300

    Added I18n to nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper(styles = '')
     if current_user
-      link_to "Logout", destroy_user_session_path, method: :delete, class: styles
+      link_to t('logout'), destroy_user_session_path, method: :delete, class: styles
     else
-      (link_to "Registered", new_user_registration_path, class: styles) +
+      (link_to t('sign_up'), new_user_registration_path, class: styles) +
           " ".html_safe +
-          (link_to "Login", new_user_session_path, class: styles)
+          (link_to t('login'), new_user_session_path, class: styles)
     end
   end
 end
