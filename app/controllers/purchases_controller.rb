51c7c657d4b38b28e1b7c66d48ce5013a73b2899
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-13 00:12:21 +0300

    Cleaned scaffold Purchase

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,13 +5,11 @@ class PurchasesController < ApplicationController
   before_action :get_products, only: [:new, :edit]
 
   # GET /purchases
-  # GET /purchases.json
   def index
     @purchases = Purchase.activated.includes(:product, :user)
   end
 
   # GET /purchases/1
-  # GET /purchases/1.json
   def show
   end
 
@@ -25,43 +23,29 @@ class PurchasesController < ApplicationController
   end
 
   # POST /purchases
-  # POST /purchases.json
   def create
     @purchase = Purchase.new(purchase_params)
 
-    respond_to do |format|
-      if @purchase.save
-        format.html { redirect_to @purchase, notice: 'Purchase was successfully created.' }
-        format.json { render :show, status: :created, location: @purchase }
-      else
-        format.html { render :new }
-        format.json { render json: @purchase.errors, status: :unprocessable_entity }
-      end
+    if @purchase.save
+      redirect_to @purchase, notice: 'Purchase was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /purchases/1
-  # PATCH/PUT /purchases/1.json
   def update
-    respond_to do |format|
-      if @purchase.update(purchase_params)
-        format.html { redirect_to @purchase, notice: 'Purchase was successfully updated.' }
-        format.json { render :show, status: :ok, location: @purchase }
-      else
-        format.html { render :edit }
-        format.json { render json: @purchase.errors, status: :unprocessable_entity }
-      end
+    if @purchase.update(purchase_params)
+      redirect_to @purchase, notice: 'Purchase was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /purchases/1
-  # DELETE /purchases/1.json
   def destroy
     @purchase.destroy
-    respond_to do |format|
-      format.html { redirect_to purchases_url, notice: 'Purchase was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to purchases_url, notice: 'Purchase was successfully destroyed.'
   end
 
   private
