1ab2d8770ec8cb54047b6cf1160779993b820678
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-13 21:07:56 +0300

    Added Implemented OpenStruct for order product

--- app/controllers/concerns/calculator_stocks.rb
+++ app/controllers/concerns/calculator_stocks.rb
@@ -1,14 +1,21 @@
 module CalculatorStocks
   extend ActiveSupport::Concern
 
+  def get_order_products
+    @orders = Order.for_user_by_period(params.fetch(:user_id), params.fetch(:period_id))
+    @user_name = User.find(params.fetch(:user_id)).name
+    @stock = Purchase.where(user_id: params.fetch(:user_id)).activated
+    @products = order_with_stock(@orders, @stock)
+  end
+
   def products_by_orders(orders)
     products = {}
     orders.each do |order|
       order.dish.ingredients.each do |ingredient|
         if products[ingredient.product_id.to_s].nil?
-          products[ingredient.product_id.to_s] = (ingredient.quantity * order.quantity * percent_of_recycling(ingredient)).to_i
+          products[ingredient.product_id.to_s] = (ingredient.quantity * order.quantity * percent_of_recycling(ingredient) * @period.days).to_i
         else
-          products[ingredient.product_id.to_s] += (ingredient.quantity * order.quantity * percent_of_recycling(ingredient)).to_i
+          products[ingredient.product_id.to_s] += (ingredient.quantity * order.quantity * percent_of_recycling(ingredient) * @period.days).to_i
         end
       end
     end
@@ -23,10 +30,19 @@ module CalculatorStocks
         products[purchase.product_id.to_s] -= (purchase.stock_left * percent_of_recycling(purchase)).to_i
       end
     end
-    products
+    order_products_for_send products
   end
 
   def percent_of_recycling(object)
     (100 + object.product.percent_of_recycling) / 100.00
+  end
+
+  def order_products_for_send(products)
+    return [] unless products
+
+    products.map do |key, value|
+      product = Product.find(key)
+      OpenStruct.new(name: product.name_with_unit, quantity: value)
+    end
   end
 end
