9c76e0e39dc7fa3864017f50dee85c237bf07c2c
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-15 00:21:33 +0300

    Implemented gritter for notification

--- Gemfile
+++ Gemfile
@@ -65,5 +65,6 @@ gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.2'
 gem 'carrierwave', '~> 1.1'
 gem 'mini_magick', '~> 4.8'
 gem 'carrierwave-aws', '~> 1.2'
+gem 'gritter', '~> 1.2'
 
 ruby "2.4.0"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
           (content_tag( tag,(link_to t('login'), new_user_session_path, class: styles)))
     end
   end
+
+  def alerts
+    alert = flash[:notice] || flash[:error] || flash[:alert]
+    if alert
+      alert_generator(alert)
+    end
+  end
+
+  def alert_generator(msg)
+    js add_gritter(msg, title: "Test Website", sticky: false)
+  end
 end
