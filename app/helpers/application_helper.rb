b5df7844f1828146820e37c348733b7bb16500aa
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-14 14:31:51 +0300

    Added style for home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper(styles = '')
+  def login_helper(tag: :li, styles: '')
     if current_user
-      link_to t('logout'), destroy_user_session_path, method: :delete, class: styles
+      content_tag( tag,(link_to t('logout'), destroy_user_session_path, method: :delete, class: styles))
     else
-      (link_to t('sign_up'), new_user_registration_path, class: styles) +
+      (content_tag( tag,(link_to t('sign_up'), new_user_registration_path, class: styles))) +
           " ".html_safe +
-          (link_to t('login'), new_user_session_path, class: styles)
+          (content_tag( tag,(link_to t('login'), new_user_session_path, class: styles)))
     end
   end
 end
