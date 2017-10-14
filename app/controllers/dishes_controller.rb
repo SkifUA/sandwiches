2117375b8590c8160fb8be7eaea4258baa552ce7
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-15 01:11:02 +0300

    Clean dishes for local

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -25,7 +25,7 @@ class DishesController < ApplicationController
     @dish = Dish.new(dish_params)
 
     if @dish.save
-      redirect_to @dish, notice: 'Dish was successfully created.'
+      redirect_to @dish, notice: t('dishes.msg.success.create')
     else
       render :new
     end
@@ -34,7 +34,7 @@ class DishesController < ApplicationController
   # PATCH/PUT /dishes/1
   def update
     if @dish.update(dish_params)
-      redirect_to @dish, notice: 'Dish was successfully updated.'
+      redirect_to @dish, notice: t('dishes.msg.success.update')
     else
       render :edit
     end
@@ -43,7 +43,7 @@ class DishesController < ApplicationController
   # DELETE /dishes/1
   def destroy
     @dish.destroy
-    redirect_to dishes_url, notice: 'Dish was successfully destroyed.'
+    redirect_to dishes_url, notice: t('dishes.msg.success.destroy')
   end
 
   private
