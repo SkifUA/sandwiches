37c1642ce44fdb8cd36a2fbdcc3fca39fdeb9b64
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-14 19:20:10 +0300

    Update styles for dish show

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -49,7 +49,7 @@ class DishesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_dish
-      @dish = Dish.find(params[:id])
+      @dish = Dish.includes(:ingredients).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
