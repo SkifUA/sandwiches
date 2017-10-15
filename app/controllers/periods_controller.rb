a9c497ca90b455541f1b5dc7cd411d4ad197c49e
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-15 11:23:59 +0300

    Updated periods

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -4,13 +4,11 @@ class PeriodsController < ApplicationController
   before_action :set_period, only: [:show, :edit, :update, :destroy]
 
   # GET /periods
-  # GET /periods.json
   def index
-    @periods = Period.all
+    @periods = Period.all.order(finish_date: :desc)
   end
 
   # GET /periods/1
-  # GET /periods/1.json
   def show
   end
 
@@ -24,43 +22,29 @@ class PeriodsController < ApplicationController
   end
 
   # POST /periods
-  # POST /periods.json
   def create
     @period = Period.new(period_params)
 
-    respond_to do |format|
-      if @period.save
-        format.html { redirect_to @period, notice: 'Period was successfully created.' }
-        format.json { render :show, status: :created, location: @period }
-      else
-        format.html { render :new }
-        format.json { render json: @period.errors, status: :unprocessable_entity }
-      end
+    if @period.save
+      redirect_to @period, notice: t('periods.msg.success.create')
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /periods/1
-  # PATCH/PUT /periods/1.json
   def update
-    respond_to do |format|
-      if @period.update(period_params)
-        format.html { redirect_to @period, notice: 'Period was successfully updated.' }
-        format.json { render :show, status: :ok, location: @period }
-      else
-        format.html { render :edit }
-        format.json { render json: @period.errors, status: :unprocessable_entity }
-      end
+    if @period.update(period_params)
+      redirect_to @period, notice: t('periods.msg.success.update')
+    else
+      render :edit
     end
   end
 
   # DELETE /periods/1
-  # DELETE /periods/1.json
   def destroy
     @period.destroy
-    respond_to do |format|
-      format.html { redirect_to periods_url, notice: 'Period was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to periods_url, notice: t('periods.msg.success.destroy')
   end
 
   private
--- app/views/periods/_period.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! period, :id, :start_date, :finish_date, :days, :created_at, :updated_at
-json.url period_url(period, format: :json)
