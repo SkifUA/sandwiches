1a3807d79a2c26348540aae3b5f3fcfd7f1a5f70
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-10-15 21:16:55 +0300

    Implemented Internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_locale
   protect_from_forgery with: :exception
   include DeviseWhiteList
   rescue_from CanCan::AccessDenied do |exception|
@@ -7,5 +9,11 @@ class ApplicationController < ActionController::Base
       format.html { redirect_to main_app.root_url, notice: exception.message }
       format.js   { head :forbidden, content_type: 'text/html' }
     end
+  end
+
+
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
