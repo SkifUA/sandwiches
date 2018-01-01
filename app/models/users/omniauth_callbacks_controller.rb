a44f3daa395036493a167879ba00cd3d1504429f
Valerii Kuzin <kuzin.valeriy@gmail.com>
2018-01-01 23:19:23 +0200

    Removed flash notis from google auth

--- app/models/users/omniauth_callbacks_controller.rb
+++ app/models/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_from_omniauth(request.env["omniauth.auth"])
     if @user
       sign_in @user
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      # flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       redirect_to root_path
     else
       redirect_to new_user_session_path, notice: 'Access Denied.'
